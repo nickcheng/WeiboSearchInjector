0d52c73873c0b13d767d936f1791ca75bee700e5
nickcheng <n@nickcheng.com>
2014-05-19 16:37:56 +0800

    Can work now.

--- README.md
+++ README.md
@@ -11,3 +11,13 @@ It's a Google Chrome extension.
 * 名人观点: http://s.weibo.com/list/coverage?search=[keyboard]&category=5
 * 实时微博: http://s.weibo.com/weibo/[keyboard]?topnav=1&wvr=5&b=1&rd=newTips
 
+## 页面注入 JQuery
+
+Chrome 下给当前网页注入 JQuery 的方法: 在 Console 里执行如下语句:
+
+	var jq = document.createElement('script');
+	jq.src = 'http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js';
+	document.getElementsByTagName('head')[0].appendChild(jq);
+	jQuery.noConflict();
+
+如果执行一遍出错, 那就再执行一遍.
--- myscript.js
+++ myscript.js
@@ -1,13 +1,58 @@
-window.onload=function(){
+window.onload = function() {
 	var feedItemList = $('dl[action-type=feed_list_item]');
+	if (feedItemList.length == 0) {
+		feedItemList = $('div[action-type=feed_list_item]');
+	}
+
 	feedItemList.each(function(idx, feed) {
-		var d = $(feed).find('.date');
-		//
+		// Get Weibo link
+		var d = $(feed).find('.date').length > 0 ? $(feed).find('.date'): $(feed).find('a[node-type="feed_list_item_date"]');
 		var link = d.last().attr("href");
+		// Get mid from link
+		linkChips = link.split('/');
+		lastChip = linkChips[linkChips.length - 1];
+		mid = lastChip.substr(0, lastChip.indexOf('?'));
+
 		//
-		var text = $(feed).find('em').first().innerText;
-		//
-		var injection = '<a href="https://api.pinboard.in/v1/posts/add?url='+link+'&description='+text+'&auth_token=">Send</a>';
-    $(feed).children('.face').append(injection);
+		// var text = $(feed).find('em').first().innerText;
+
+		// Build injection
+		// var injection = '<a href="https://api.pinboard.in/v1/posts/add?url='+link+'&description='+text+'&auth_token=">Send</a>';
+		var injection = $(document.createElement('button'));
+		injection.text('Add This');
+		injection.click({'mid':mid, 'feed':$(feed).clone()}, addOverlay);
+			// {
+			//	text: ,
+			//	click: function() { addOverlay(mid); }
+			// });
+
+		// Inject to page under the avatar
+    var injected = $(feed).children('.face');
+    if (injected.length == 0)
+			injected = $($(feed).find('.WB_face').get(0));
+    injected.append(injection);
 	});
+}
+
+function addOverlay(event) {
+	// info = 'Hi again! ' + event.data.mid
+	// alert(info);
+
+	//
+	mid = event.data.mid;
+	feed = event.data.feed;
+
+	// Define background overlay
+	var overlay = $(document.createElement('div'));
+	overlay.attr('class', 'overlay');
+	overlay.click(function() {
+		$(this).remove();
+	});
+
+	//
+	feed.attr('class', 'feed');
+	overlay.append(feed);
+
+	// Added overlay to page
+	$(document.body).append(overlay);
 }
